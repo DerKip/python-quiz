35ba65125679e066b6f4fec8b407dc49639e8319
Derrick Kipchumba <dkip64@gmail.com>
2018-02-15 13:56:04 +0300

    Update second_largest.py

--- second_largest.py
+++ second_largest.py
@@ -6,13 +6,13 @@ def second_largest(li):
         if largest==None:
             largest=li[i]
         elif li[i]>largest:
-            second_largest=largest
+            second_largest=largest #second_largest initialized with largest value before largest gets updated to the next largest element
             largest=li[i]
         elif li[i]>second_largest:
             second_largest=li[i]
     return second_largest
 
-print(second_largest([2,3,4469,6,565]))
+# print(second_largest([2,3,4469,6,565]))   ---For Testing-----
 
 
 
